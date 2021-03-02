8c96eaf6932b06946d5ea2e945f92004a6beeb81
meghadas1221 <78722705+meghadas1221@users.noreply.github.com>
2021-03-02 23:53:42 +0530

    React app Added comment

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import "./styles.css";
 import { useState } from "react";
 
+// react app
 // var likeCounter = 0;
 export default function App() {
-
-[likeCounter, setLikeCounter] = useState(0)
-function likeClickHandler() {
-  setLikeCounter(likeCounter + 1)
-}
+  [likeCounter, setLikeCounter] = useState(0);
+  function likeClickHandler() {
+    setLikeCounter(likeCounter + 1);
+  }
 
   return (
     <div className="App">
       <h1>Inside Out</h1>
-      <button onClick = {likeClickHandler}>Like Me!</button>{likeCounter}
+      <button onClick={likeClickHandler}>Like Me!</button>
+      {likeCounter}
     </div>
   );
 }
